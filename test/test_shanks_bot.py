afe275a5c0ce7810ab13bb9252e145b55dd79971
Graham Strickland <grahamhstrickland@gmail.com>
2024-12-30 10:47:25 +0200

    Add parametrization to shanks_bot test

--- test/test_shanks_bot.py
+++ test/test_shanks_bot.py
@@ -1,59 +1,20 @@
 #!/usr/bin/env python3
+import pytest
 
 from src.shanks_bot import shanks_bot
 
 
-def test_shanks_bot() -> None:
-    p = 7
-    obs = shanks_bot(p)
-    actual = 6
-
-    assert obs == actual
-
-
-def test_shanks_bot2() -> None:
-    p = 23
-    obs = shanks_bot(p)
-    actual = 20
-
-    assert obs == actual
-
-
-def test_shanks_bot3() -> None:
-    p = 60013
-    obs = shanks_bot(p)
-    actual = 5001
-
-    assert obs == actual
-
-
-def test_shanks_bot4() -> None:
-    p = 60017
-    obs = shanks_bot(p)
-    actual = 60016
-
-    assert obs == actual
-
-
-def test_shanks_bot5() -> None:
-    p = 61141
-    obs = shanks_bot(p)
-    actual = 12228
-
-    assert obs == actual
-
-
-def test_shanks_bot6() -> None:
-    p = 62057
-    obs = shanks_bot(p)
-    actual = 62056
-
-    assert obs == actual
-
-
-def test_shanks_bot7() -> None:
-    p = 61547
-    obs = shanks_bot(p)
-    actual = 30773
-
-    assert obs == actual
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (7, 6),
+        (23, 20),
+        (60013, 5001),
+        (60017, 60016),
+        (61141, 12228),
+        (62057, 62056),
+        (61547, 30773),
+    ],
+)
+def test_shanks_bot(input: int, expected: int) -> None:
+    assert shanks_bot(input) == expected
