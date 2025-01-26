b20b3cae06090b8a52f4fa9aac7cf6f21b93e444
Graham Strickland <grahamhstrickland@gmail.com>
2025-01-26 14:57:54 +0200

    Fix Lorenz system script

--- src/plots/lorenz_system.py
+++ src/plots/lorenz_system.py
@@ -7,16 +7,21 @@ sigma = 10.0
 beta = 8.0 / 3.0
 
 
-def f(state, t):
-    x, y, z = state  # Unpack the state vector
-    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
+def f(state: list[float], t: np.ndarray) -> tuple[float]:
+    x, y, z = state
+    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z
 
 
-state0 = [1.0, 1.0, 1.0]
-t = np.arange(0.0, 40.0, 0.01)
+def main() -> None:
+    state0 = [1.0, 1.0, 1.0]
+    t = np.arange(0.0, 40.0, 0.01)
 
-states = odeint(f, state0, t)
+    states = odeint(f, state0, t)
 
-ax = plt.figure().add_subplot(projection="3d")
-ax.plot(states[:, 0], states[:, 1], states[:, 2])
-plt.show()
+    ax = plt.figure().add_subplot(projection="3d")
+    ax.plot(states[:, 0], states[:, 1], states[:, 2])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
